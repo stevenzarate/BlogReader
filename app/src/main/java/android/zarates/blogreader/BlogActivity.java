f4fd4e4f0f7bf58b03aad1e3c4cfc2b53e6534bb
steven <stevengzarate68@gmail.com>
2014-10-05 20:18:10 -0700

    accessed progress bar and list view

--- app/src/main/java/android/zarates/blogreader/BlogActivity.java
+++ app/src/main/java/android/zarates/blogreader/BlogActivity.java
@@ -2,35 +2,38 @@ package android.zarates.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 
 public class BlogActivity extends Activity {
+
+    protected ProgressBar progressBar;
+    protected ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+
+        String[] arrayStrings = new String[] {
+                "BLOG POST 1",
+                "BLOG POST 2",
+                "BLOG POST 3",
+                "BLOG POST 4",
+                "BLOG POST 5"
+        };
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        listView = (ListView)findViewById(R.id.listView);
+
+        listView.setEmptyView(progressBar);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+        listView.setAdapter(adapter);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.blog, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
