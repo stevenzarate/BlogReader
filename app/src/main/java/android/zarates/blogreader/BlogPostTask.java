bb494368794ac95f3f44b3b3aef77acb4d75f820
steven <stevengzarate68@gmail.com>
2014-10-05 20:44:03 -0700

    stored the blog

--- app/src/main/java/android/zarates/blogreader/BlogPostTask.java
+++ app/src/main/java/android/zarates/blogreader/BlogPostTask.java
@@ -2,12 +2,22 @@ package android.zarates.blogreader;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     @Override
-    protected JSONObject doInBackground(Activity... params) {
+    protected JSONObject doInBackground(Activity... activities) {
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+        }
+        catch (MalformedURLException error){
+            Log.e("BlogPostTask", "Malformed URL: " + error);
+        }
         return null;
     }
 }
