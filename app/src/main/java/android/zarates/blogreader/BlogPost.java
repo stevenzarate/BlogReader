78873b01e269624c2216337fffe84f5307f0053c
steven <stevengzarate68@gmail.com>
2014-10-05 20:20:56 -0700

    made blog post class

---
+++ app/src/main/java/android/zarates/blogreader/BlogPost.java
@@ -0,0 +1,11 @@
+package android.zarates.blogreader;
+
+public class BlogPost {
+    public final String title;
+    public final String url;
+
+    public BlogPost(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+}
