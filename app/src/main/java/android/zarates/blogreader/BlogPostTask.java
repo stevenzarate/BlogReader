9b9a05484ede04946b31bb4352f8687d79ef7d1d
steven <stevengzarate68@gmail.com>
2014-10-05 20:32:27 -0700

    created blog post task

---
+++ app/src/main/java/android/zarates/blogreader/BlogPostTask.java
@@ -0,0 +1,13 @@
+package android.zarates.blogreader;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
+    @Override
+    protected JSONObject doInBackground(Activity... params) {
+        return null;
+    }
+}
