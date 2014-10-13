a0be09dcebe62028fb7b634e2e84cb446fc2c5cb
steven <stevengzarate68@gmail.com>
2014-10-13 09:59:19 -0700

    Overriding and Understanding getView

--- app/src/main/java/android/zarates/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/zarates/blogreader/BlogPostAdapter.java
@@ -1,6 +1,8 @@
 package android.zarates.blogreader;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -8,5 +10,10 @@ import java.util.ArrayList;
 public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
     public BlogPostAdapter(Context context,ArrayList<BlogPost> posts) {
         super(context, 0, posts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        
     }
 }
