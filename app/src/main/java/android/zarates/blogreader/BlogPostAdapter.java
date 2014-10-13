bcf6c22ee39b452ecfb3c5299f5d22c1075a6c68
steven <stevengzarate68@gmail.com>
2014-10-13 13:57:08 -0700

    Accessing the TextView to Set the Title

--- app/src/main/java/android/zarates/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/zarates/blogreader/BlogPostAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,5 +21,10 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         }
+
+        TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
+        textView.setText(post.title);
+
+        return convertView;
     }
 }
