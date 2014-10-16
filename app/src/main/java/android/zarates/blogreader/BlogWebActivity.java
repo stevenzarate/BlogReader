f3c0b1c8000a433d4e180521c0bc660f6a2255e8
steven <stevengzarate68@gmail.com>
2014-10-16 13:51:52 -0700

    Retrieving an Intent to Start an Activity

--- app/src/main/java/android/zarates/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/zarates/blogreader/BlogWebActivity.java
@@ -1,12 +1,22 @@
 package android.zarates.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class BlogWebActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
+
+        Intent intent = getIntent();
+        Uri blogUri = intent.getData();
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.loadUrl(blogUri.toString());
+
     }
 }
