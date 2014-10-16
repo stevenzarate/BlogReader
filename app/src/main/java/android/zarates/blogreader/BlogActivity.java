bca9089e7eae890b9ec8accaf4b443072dc49497
steven <stevengzarate68@gmail.com>
2014-10-16 13:46:21 -0700

    Starting an Activity Using Intents

--- app/src/main/java/android/zarates/blogreader/BlogActivity.java
+++ app/src/main/java/android/zarates/blogreader/BlogActivity.java
@@ -1,6 +1,8 @@
 package android.zarates.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -31,6 +33,12 @@ public class BlogActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("BlogActivity", "Title: " + BlogPostParser.get().posts.get(position).title);
+
+                Intent intent = new Intent(getApplicationContext(), BlogWebActivity.class);
+                Uri blogUri = Uri.parse(BlogPostParser.get().posts.get(position).url);
+                intent.setData(blogUri);
+
+                startActivity(intent);
             }
         });
 
