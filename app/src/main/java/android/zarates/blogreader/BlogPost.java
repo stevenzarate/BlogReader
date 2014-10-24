914e03119cd4e75a3c1d261ce8d6d51ace8ae9e6
steven <stevengzarate68@gmail.com>
2014-10-24 13:48:54 -0700

    Store All Post Information within the Blog Post Class

--- app/src/main/java/android/zarates/blogreader/BlogPost.java
+++ app/src/main/java/android/zarates/blogreader/BlogPost.java
@@ -3,10 +3,16 @@ package android.zarates.blogreader;
 public class BlogPost {
     public final String title;
     public final String url;
+    public final String date;
+    public final String author;
+    public final String thumbnail;
 
-    public BlogPost(String title, String url) {
+    public BlogPost(String title, String url, String date, String author, String thumbnail) {
         this.title = title;
         this.url = url;
+        this.date = date;
+        this.author = author;
+        this.thumbnail = thumbnail;
     }
 
 }
