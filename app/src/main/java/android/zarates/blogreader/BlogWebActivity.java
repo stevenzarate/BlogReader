1bc9e4ea7b393eb97ec5b0d19cf5157eccb3528c
steven <stevengzarate68@gmail.com>
2014-11-04 13:54:46 -0800

    put progress bar on web activity

--- app/src/main/java/android/zarates/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/zarates/blogreader/BlogWebActivity.java
@@ -4,13 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity {
+    protected ProgressBar progressBar;
+    protected WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBarWeb);
+        webView = (WebView)findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
 
         Intent intent = getIntent();
         Uri blogUri = intent.getData();
